8e8b647a865705f3d80fc81e37b8dbc93efc50e8
Tejas Mane <tejasmane485@gmail.com>
2022-12-21 16:53:01 +0530

    project model created

---
+++ backend/apps/daily-status-app/project-micro-app/models/projectModel.js
@@ -0,0 +1,15 @@
+module.exports = (sequlize, type) => {
+    return sequlize.define("projects", {
+        project_id: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        emp_id: type.INTEGER,
+        project_name: type.STRING,
+        project_start_dt: type.TIMESTAMP,
+        project_end_dt: type.TIMESTAMP,
+        project_manager_name: type.STRING,
+        project_manager_email: type.STRING,
+    })
+}
