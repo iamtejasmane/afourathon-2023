cb82f428f83d151ef10060ca2842d39d3197e4db
vijay-t13 <vijay.tembugade@intangles.com>
2022-12-22 17:22:44 +0530

    adding add new project btn and projects list table

--- frontend/daily-status-app/src/index.js
+++ frontend/daily-status-app/src/index.js
@@ -3,27 +3,30 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    type: "light",
+    type: 'light',
     primary: {
-      main: "#146073",
+      main: '#0277bd',
+      light: '#e8f1fa',
     },
     secondary: {
-      main: "#EF7B45",
+      main: '#f19d33',
+      dark: '#f18b05',
+      light: '#dcb685',
     },
   },
 });
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App/>
     </ThemeProvider>
   </React.StrictMode>
 );
