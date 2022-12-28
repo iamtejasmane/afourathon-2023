94f6dd41f1713940290b2cf51a5b45576916e8c0
Tejas Mane <tejasmane485@gmail.com>
2022-12-28 22:44:54 +0530

    create and update employee api's

--- backend/apps/employee-management-app/emp-micro-app/routes/employee.js
+++ backend/apps/employee-management-app/emp-micro-app/routes/employee.js
@@ -4,16 +4,7 @@ const utils = require("../utils/utils")
 const { Employees } = require("../../../db-server/db/db-mysql")
 const router = express.Router()
 
-router.get("/employees", (req, res) => {
-  Employees.findAll()
-    .then((employees) => {
-      res.send(utils.createResult(null, employees))
-    })
-    .catch((err) => {
-      res.send(utils.createResult(err, null))
-    })
-})
-
+// get employee details api
 router.get("/employees/:id", (req, res) => {
   const emp_id = req.params.id
 
@@ -26,6 +17,32 @@ router.get("/employees/:id", (req, res) => {
     })
 })
 
-router.post("/employees", (req, res) => {})
+// add new employee details api
+router.post("/employees", (req, res) => {
+  const { first_name, last_name, gender, email, password, mobile } = req.body
+
+  Employees.create({
+    first_name: first_name,
+    last_name: last_name,
+    gender: gender,
+    email: email,
+    password: password,
+    mobile: mobile,
+  })
+})
+
+// update employee information api
+router.put("/employees/:id", (req, res) => {
+  Employees.findByPk(req.params.id)
+    .then((employee) => {
+      employee.update(req.body)
+    })
+    .then((employee) => {
+      res.send(utils.createResult(null, employee))
+    })
+    .catch((err) => {
+      res.send(utils.createResult(err, null))
+    })
+})
 
 module.exports = router
