27c02d143c023a320b44bc5f4334a87a29a9a9cd
Tejas Mane <tejasmane485@gmail.com>
2022-12-28 16:46:55 +0530

    employee get api added

--- backend/apps/employee-management-app/emp-micro-app/routes/employee.js
+++ backend/apps/employee-management-app/emp-micro-app/routes/employee.js
@@ -4,4 +4,28 @@ const utils = require("../utils/utils")
 const { Employees } = require("../../../db-server/db/db-mysql")
 const router = express.Router()
 
+router.get("/employees", (req, res) => {
+  Employees.findAll()
+    .then((employees) => {
+      res.send(utils.createResult(null, employees))
+    })
+    .catch((err) => {
+      res.send(utils.createResult(err, null))
+    })
+})
+
+router.get("/employees/:id", (req, res) => {
+  const emp_id = req.params.id
+
+  Projects.findAll({ where: { emp_id: emp_id } })
+    .then((employee) => {
+      res.send(utils.createResult(null, employee))
+    })
+    .catch((err) => {
+      res.send(utils.createResult(err, null))
+    })
+})
+
+router.post("/employees", (req, res) => {})
+
 module.exports = router
