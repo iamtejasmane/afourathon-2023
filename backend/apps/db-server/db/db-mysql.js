7966d02b49a0dbc4cf2021c94fa7e99fea9ae218
Tejas Mane <tejasmane485@gmail.com>
2022-12-28 15:58:08 +0530

    employee model updated

--- backend/apps/db-server/db/db-mysql.js
+++ backend/apps/db-server/db/db-mysql.js
@@ -1,8 +1,9 @@
-const Sequelize = require("sequelize");
-const PORT = process.env.PORT || "3306";
+const Sequelize = require("sequelize")
+const PORT = process.env.PORT || "3306"
 
 // import table strcuture from the models
-const projectModel = require("../models/projectModel");
+const projectModel = require("../models/projectModel")
+const employeeModel = require("../models/employeeModel")
 
 // a new instance of the Sequelize to access the database
 const sequelize = new Sequelize("afourathon", "root", "password", {
@@ -15,30 +16,32 @@ const sequelize = new Sequelize("afourathon", "root", "password", {
     acquire: 30000,
     idle: 10000,
   },
-});
+})
 
 // authenticate the database access
 // and establish a connection
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connection established successfully");
+    console.log("Connection established successfully")
   })
   .catch((error) => {
-    console.log("error " + error);
-  });
+    console.log("error " + error)
+  })
 
 // creates an instance of a table
 
-const Projects = projectModel(sequelize, Sequelize);
+const Employees = employeeModel(sequelize, Sequelize)
+const Projects = projectModel(sequelize, Sequelize)
 
 // database schema relationships
+Employees.hasMany(Projects, { foreignKey: "emp_id", sourceKey: "project_id" })
 
 // synchronize the connection
 sequelize.sync({ force: false }).then(() => {
-  console.log("Database synced");
-});
+  console.log("Database synced")
+})
 
 module.exports = {
   Projects: Projects,
-};
+}
