ac29576dfa940d96fa9af68a5c59121f6ff38094
Tejas Mane <tejasmane485@gmail.com>
2022-12-28 14:34:30 +0530

    models migrated

--- backend/apps/db-server/db/db-mysql.js
+++ backend/apps/db-server/db/db-mysql.js
@@ -1,47 +1,44 @@
-const Sequelize = require("sequelize")
-const PORT = process.env.PORT || '3306'
+const Sequelize = require("sequelize");
+const PORT = process.env.PORT || "3306";
 
 // import table strcuture from the models
-const projectModel = require("../../daily-status-app/project-micro-app/models/projectModel")
+const projectModel = require("../models/projectModel");
 
 // a new instance of the Sequelize to access the database
 const sequelize = new Sequelize("afourathon", "root", "password", {
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port: PORT,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  })
+  host: "127.0.0.1",
+  dialect: "mysql",
+  port: PORT,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 
 // authenticate the database access
 // and establish a connection
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connection established successfully")
+    console.log("Connection established successfully");
   })
   .catch((error) => {
-    console.log("error " + error)
-  })
-
-
+    console.log("error " + error);
+  });
 
 // creates an instance of a table
 
-const Projects = projectModel(sequelize, Sequelize) 
+const Projects = projectModel(sequelize, Sequelize);
 
 // database schema relationships
 
-
 // synchronize the connection
 sequelize.sync({ force: false }).then(() => {
-    console.log("Database synced")
-  })
+  console.log("Database synced");
+});
 
 module.exports = {
-    Projects: Projects
-  }
+  Projects: Projects,
+};
