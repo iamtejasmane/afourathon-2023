4daae66f836acfc1985ac1a14edf51e044b3571b
Tejas Mane <tejasmane485@gmail.com>
2023-01-02 13:40:41 +0530

    create project api

--- backend/apps/daily-status-app/project-micro-app/routes/project.js
+++ backend/apps/daily-status-app/project-micro-app/routes/project.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const utils = require("../utils/utils")
 
-const { Projects } = require("../../../db-server/db/db-mysql")
+const { Projects, Employees } = require("../../../db-server/db/db-mysql")
+
 const router = express.Router()
 
 router.get("/projects", (req, res) => {
@@ -26,6 +27,41 @@ router.get("/projects/:id", (req, res) => {
     })
 })
 
-router.post("/projects", (req, res) => {})
+router.post("/projects/:id", async (req, res) => {
+  const emp_id = req.params.id
+  const {
+    project_name,
+    project_start_dt,
+    project_end_dt,
+    project_manager_name,
+    project_manager_email,
+  } = req.body
+
+  const employee = await Employees.findByPk(emp_id)
+
+  // check if the employee the employee has required permissions and role.
+  // if (employee["designation"] == "Project Manager" && role > 4) { // for role based access
+  if (employee["designation"] == "Project Manager") {
+    Projects.create({
+      emp_id: emp_id,
+      project_name: project_name,
+      project_start_dt: project_start_dt,
+      project_end_dt: project_end_dt,
+      project_manager_name: project_manager_name,
+      project_manager_email: project_manager_email,
+    })
+      .then((projects) => {
+        res.send(utils.createResult(null, projects))
+      })
+      .catch((err) => {
+        res.send(utils.createResult(err, null))
+      })
+  } else {
+    res.status(404).json({
+      status: "error",
+      error: "Permission denied!",
+    })
+  }
+})
 
 module.exports = router
