ae117afc6514746a71bb0fda3d461a6c4c09a265
Tejas Mane <tejasmane485@gmail.com>
2023-01-02 14:50:05 +0530

    project update and delete apis

--- backend/apps/daily-status-app/project-micro-app/routes/project.js
+++ backend/apps/daily-status-app/project-micro-app/routes/project.js
@@ -5,26 +5,41 @@ const { Projects, Employees } = require("../../../db-server/db/db-mysql")
 
 const router = express.Router()
 
-router.get("/projects", (req, res) => {
-  Projects.findAll()
-    .then((projects) => {
-      res.send(utils.createResult(null, projects))
-    })
-    .catch((err) => {
-      res.send(utils.createResult(err, null))
-    })
-})
+// This api is for admin users to get all the projects in the database
+// router.get("/projects", (req, res) => {
+//   Projects.findAll()
+//     .then((projects) => {
+//       res.send(utils.createResult(null, projects))
+//     })
+//     .catch((err) => {
+//       res.send(utils.createResult(err, null))
+//     })
+// })
 
-router.get("/projects/:id", (req, res) => {
-  const project_id = req.params.id
+// get all the projects created by employee
+router.get("/projects/:id", async (req, res) => {
+  const emp_id = req.params.id
 
-  Projects.findAll({ where: { project_id: project_id } })
-    .then((project) => {
-      res.send(utils.createResult(null, project))
+  let employee = await Employees.findByPk(emp_id)
+  // employee = JSON.stringify(employee, null, 2)
+  console.log(employee)
+  // check if the employee has required permissions and role.
+  // if (employee["designation"] == "Project Manager" && role > 4) { // for role based access
+  if (employee["designation"] == "Project Manager") {
+    console.log("here".red)
+    Projects.findAll({ where: { emp_id: emp_id } })
+      .then((project) => {
+        res.send(utils.createResult(null, project))
+      })
+      .catch((err) => {
+        res.send(utils.createResult(err, null))
+      })
+  } else {
+    res.status(404).json({
+      status: "error",
+      error: "Permission denied!",
     })
-    .catch((err) => {
-      res.send(utils.createResult(err, null))
-    })
+  }
 })
 
 router.post("/projects/:id", async (req, res) => {
@@ -62,6 +77,39 @@ router.post("/projects/:id", async (req, res) => {
       error: "Permission denied!",
     })
   }
+})
+
+// update project information api
+// id: project id to be updated
+router.put("/projects/:id", (req, res) => {
+  const project_id = req.params.id
+
+  Projects.findByPk(project_id)
+    .then(async (employee) => {
+      await employee.update(req.body)
+    })
+    .then((employee) => {
+      res.send(utils.createResult(null, employee))
+    })
+    .catch((err) => {
+      res.send(utils.createResult(err, null))
+    })
+})
+
+// delete project by id
+router.delete("/projects/:id", (req, res) => {
+  const project_id = req.params.id
+
+  Projects.findByPk(project_id)
+    .then(async (employee) => {
+      await employee.destroy()
+    })
+    .then((employee) => {
+      res.send(utils.createResult(null, employee))
+    })
+    .catch((err) => {
+      res.send(utils.createResult(err, null))
+    })
 })
 
 module.exports = router
--- backend/apps/daily-status-app/project-micro-app/server.js
+++ backend/apps/daily-status-app/project-micro-app/server.js
@@ -13,5 +13,5 @@ app.use(morgan("combined"))
 app.use(routerProject)
 
 app.listen(8000, () => {
-  console.log("project server listening on port 8000")
+  console.log("project server is listening on port: ".green + "8000".blue)
 })
