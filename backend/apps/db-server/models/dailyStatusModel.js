426e81a50a1a1dca78e6a0ab96bb2b391e6b3a6a
Tejas Mane <tejasmane485@gmail.com>
2023-01-03 11:55:23 +0530

    status model created

---
+++ backend/apps/db-server/models/dailyStatusModel.js
@@ -0,0 +1,14 @@
+module.exports = (sequlize, type) => {
+  return sequlize.define("daily_status", {
+    status_id: {
+      type: type.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    emp_id: type.INTEGER,
+    ticket_id: type.STRING,
+    hours_spent: type.INTEGER,
+    status: type.STRING,
+    comments: type.TEXT,
+  })
+}
