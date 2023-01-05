a7132336ca76336a4603ff399d2e5645e048e2e0
Tejas Mane <tejasmane485@gmail.com>
2023-01-05 08:30:32 +0530

    weekly status app server created

---
+++ backend/apps/weekly-status-app/project-micro-app/server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+
+const routerProject = require("./routes/project")
+const routerWeeklyStatusEMails = require("./routes/weeklyStatusEmails")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors("*"))
+app.use(morgan("combined"))
+
+// Uncomment the project router line if project app of the daily status app
+// is running on the different server
+
+// app.use(routerProject)
+app.use(routerWeeklyStatusEMails)
+
+app.listen(8020, () => {
+  console.log(
+    "weekly status project server is listening on port: ".green + "8020".blue
+  )
+})
